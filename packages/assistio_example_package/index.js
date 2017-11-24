6925f7448d5f3256802917a79791f0a35cf44bd7
Steffen Sullivan <steffen2012@gmail.com>
2017-11-24 11:27:06 -0600

    added assistio_example_package

---
+++ packages/assistio_example_package/index.js
@@ -0,0 +1,10 @@
+const apiai = require('apiai');
+
+module.exports = testFunction;
+
+function testFunction(data) {
+  var returnData = {};
+  returnData.ttsText = "Test complete";
+  return returnData
+}
+//TODO Get application names and main functions
